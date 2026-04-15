60c660484954407cf18b82f7af13fbf7b566cc6a
FavieCodes <imo.e.udoh@gmail.com>
2026-04-15 14:56:12 +0100

    Update on the api documentation and admin dashboard

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'gender', 'age', 'age_group',
+        'country_id', 'gender_probability', 'country_probability', 'created_at'
+    )
+    list_filter = ('gender', 'age_group', 'country_id')
+    search_fields = ('name', 'country_id')
+    ordering = ('-created_at',)
+    readonly_fields = (
+        'id', 'name', 'gender', 'gender_probability', 'sample_size',
+        'age', 'age_group', 'country_id', 'country_probability', 'created_at'
+    )
+
+    def has_add_permission(self, request):
+        return False  
